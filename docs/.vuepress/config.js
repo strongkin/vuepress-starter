691c25e1812b680758bab82a091064b1dba3b1e4
橘橙天下 <2279345175@qq.com>
2022-10-12 11:56:17 +0800

    one

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: "TS文档",
+  description: "TS文档翻译",
+  base: "/vuepress-starter/",
+  themeConfig: {
+    // logo: "/assets/img/logo.png",
+    nav: [
+      { text: "首页", link: "/" },
+      {
+        text: " JavaScript 博客",
+        items: [
+          { text: "Github", link: "https://github.com/strongkin" },
+          {
+            text: "掘金",
+            link: "https://juejin.cn",
+          },
+        ],
+      },
+    ],
+  },
+}
---
+++ docs/README.md
@@ -0,0 +1,6 @@
+---
+title: 条件类型
+author: strongking
+date: '2021-12-12'
+---
+'# Hello VuePress' 
